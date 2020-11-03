336028c203c9cc13eb75444eac06a58905b5d7e6
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-11-03 19:01:32 +0900

    HeroServiceとMessageServiceのテスト追加

--- __tests__/hero.service.spec.ts
+++ __tests__/hero.service.spec.ts
@@ -1,8 +1,11 @@
 
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { HeroService } from 'src/app/hero.service';
 import { MessageService } from 'src/app/message.service';
+import { HEROES } from 'src/app/mock-heroes';
 
+// 外部モジュールのモック化
 jest.mock('src/app/message.service');
 
 describe('TestHeroService', () => {
@@ -17,5 +20,32 @@ describe('TestHeroService', () => {
 
     it('should be created', () => {
         expect(heroService).toBeTruthy();
+    });
+
+    describe('getHeroesメソッドのテスト', () => {
+        it('モック用の配列を返却すること', () => {
+            // 準備
+            const expectedResult = HEROES;
+
+            // 実行
+            const result$ = heroService.getHeroes();
+
+            // 検証
+            result$.subscribe(result => {
+                expect(result).toEqual(expectedResult);
+            });
+        });
+
+        it('MessageServiceのaddメソッドを呼び出していること', () => {
+            // 準備
+            const messageService = TestBed.inject(MessageService);
+            const spy = jest.spyOn(messageService, 'add');
+
+            // 実行
+            heroService.getHeroes();
+
+            // 検証
+            expect(spy).toBeCalled();
+        });
     });
 });
--- __tests__/message.service.spec.ts
+++ __tests__/message.service.spec.ts
@@ -3,16 +3,55 @@ import { MessageService } from 'src/app/message.service';
 
 describe('MessageServiceのテスト', () => {
     let messageService: MessageService;
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [
-            ]
-        });
 
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
         messageService = TestBed.inject(MessageService);
     });
 
-    it('インスタンス化', () => {
+    it('インスタンス化されること', () => {
         expect(messageService).toBeTruthy();
+    });
+
+    it('初期化時はmessagesプロパティは空配列', () => {
+        expect(messageService.messages).toEqual([]);
+    });
+
+    describe('addメソッド', () => {
+        it('messagesプロパティに追加されるか', () => {
+            // 準備
+            const message = 'hello world.';
+
+            // 実行
+            messageService.add(message);
+
+            // 検証
+            expect(messageService.messages).toEqual([message]);
+        });
+        it('複数回追加した場合', () => {
+            // 準備
+            const message1 = 'hello';
+            const message2 = 'world.';
+
+            // 実行
+            messageService.add(message1);
+            messageService.add(message2);
+
+            // 検証
+            expect(messageService.messages).toEqual([message1, message2]);
+        });
+    });
+
+    describe('clearメソッド', () => {
+        it('messagesが空配列になるか', () => {
+            // 準備
+            messageService.messages = ['test', 'message'];
+
+            // 実行
+            messageService.clear();
+
+            // 検証
+            expect(messageService.messages).toEqual([]);
+        });
     });
 });
